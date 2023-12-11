87f151bcc0736f352b9d6ecced2f4dd4d5c651a7
fmssa <mssa-saidi_f@hotmail.fr>
2023-12-11 16:25:02 +0100

    add library

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Call the element loader before the bootstrapModule/bootstrapApplication call
+defineCustomElements(window);
 
 if (environment.production) {
   enableProdMode();
